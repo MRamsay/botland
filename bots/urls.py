1aef14607881a73ae9a3e4745c154ba32541c84c
Michael Ramsay <michael.ramsay@farmlinksolutions.ca>
2021-10-24 11:31:39 -0500

    cleaner regex

--- bots/urls.py
+++ bots/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dantebot/', views.canto_index, name='index'),
-    path('dantebot/<int:canto>/', views.dantebot, name='dantebot'),
+    path('inferno/', views.canto_index, name='inferno_index'),
+    path('inferno/<int:canto>/', views.dantebot, name='inferno_canto'),
 ]
--- bots/views.py
+++ bots/views.py
@@ -1,3 +1,4 @@
+from typing import Pattern
 from django import template
 from django.http.response import HttpResponse
 from django.core.handlers.wsgi import WSGIRequest
@@ -49,35 +50,54 @@ def canto_index(request: WSGIRequest):
 
 def dantebot(request: WSGIRequest, canto: int = 1):
 
+    # --- Content load
+
     path = 'bots/content/dantebot'
     english_file = f'{path}/canto_{canto}_en.md'
     italian_file = f'{path}/canto_{canto}_it.md'
     footnotes_file = f'{path}/canto_{canto}_en_footnotes.md'
 
     with open(english_file, 'r') as f:
-        english = f.read()
+        english: str = f.read()
 
     with open(italian_file, 'r') as f:
-        italian = f.read()
+        italian: str = f.read()
 
     with open(footnotes_file, 'r') as f:
-        footnotes = f.read()
+        footnotes: str = f.read()
 
-    # Preprocessing
-    english = re.sub('\[(\d*)\]', '[^\g<1>]', english)
-    english = re.sub('\n\s\s(\S)', r'\n\n\g<1>', english) # Every triplet starts without indenting, use this to add whitespace
-    footnotes = re.sub('\[(\d*)\]', '[^\g<1>]:', footnotes)
 
-    marked_up_footnotes = f'{english}\n{footnotes}'
+    # --- Preprocessing to get text display ready
 
-    # Link footnotes to English
-    marked_up_footnotes = markdown(marked_up_footnotes, extensions=['footnotes'])
-    splits = re.split(r'(?=<div class="footnote")', marked_up_footnotes)
-    english = splits[0]
-    footnotes = ''.join(splits[1])
-
+    # Original Italian is easy to preprocess.
     italian = markdown(italian)
     italian = re.sub('\n', '</br>', italian)
+
+    # English translation w/ footnotes needs more work
+
+    # Transform footnote style given into the markdown extension for footnotes style.
+    # https://www.markdownguide.org/extended-syntax/#footnotes
+
+    english_footnote_re: Pattern = re.compile(r'\[(\d*)\]')
+    english_footnote_post_re = r'[^\g<1>]:'
+    english = re.sub(english_footnote_re, english_footnote_post_re, english)
+    footnotes = re.sub(english_footnote_re, english_footnote_post_re, footnotes)
+
+    # Add a newline to each verse triplet
+    # NOTE: Every verse triplet starts without indenting
+    start_of_english_triplet_re: Pattern = re.compile(r'\n\s\s(\S)')
+    an_extra_newline_re = r'\n\n\g<1>'
+    english = re.sub(start_of_english_triplet_re, an_extra_newline_re, english) 
+
+    marked_up_footnotes: str = f'{english}\n{footnotes}'
+
+    # Links footnotes to English as well
+    marked_up_footnotes = markdown(marked_up_footnotes, extensions=['footnotes'])
+
+    english_footnote_splitpoint: Pattern = re.compile(r'(?=<div class="footnote")')
+    splits = re.split(english_footnote_splitpoint, marked_up_footnotes)
+    english, footnotes = splits
+
     english = re.sub('\n', '</br>', english)
 
     template = loader.get_template('bots/canto.html')
