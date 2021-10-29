3f95b57f578253071569c3e8b436cdbb617d28a4
Michael Ramsay <michael.ramsay@farmlinksolutions.ca>
2021-10-28 20:27:48 -0500

    settings change

--- botland/settings.py
+++ botland/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # This is not stored in source control!
 from .secrets import *
@@ -124,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
